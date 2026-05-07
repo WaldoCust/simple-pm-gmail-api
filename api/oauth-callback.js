c44d83549afc7dbf94a8e048cf9cb7c62c38aebf
WaldoCust <waldocust@gmail.com>
2026-05-07 11:59:58 -0400

    Update oauth-callback.js

--- api/oauth-callback.js
+++ api/oauth-callback.js
@@ -1,12 +1,10 @@
 import { google } from "googleapis";
 
 export default async function handler(req, res) {
-  try {
-    const code = req.query.code;
 
-    if (!code) {
-      return res.status(400).send("Missing OAuth code");
-    }
+  try {
+
+    const code = req.query.code;
 
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
@@ -16,34 +14,19 @@ export default async function handler(req, res) {
 
     const { tokens } = await oauth2Client.getToken(code);
 
-    console.log("TOKENS:", tokens);
+    console.log("TOKENS:", JSON.stringify(tokens, null, 2));
 
-    oauth2Client.setCredentials(tokens);
+    return res.send(`
+      <h1>OAuth Success</h1>
+      <pre>${JSON.stringify(tokens, null, 2)}</pre>
+    `);
 
-    // IMPORTANT:
-    // Save refresh token somewhere persistent
-    // For now temporarily store in env/database/etc
-
-    if (!tokens.refresh_token) {
-      console.error("NO REFRESH TOKEN RETURNED");
-
-      return res
-        .status(500)
-        .send("No refresh token returned from Google");
-    }
-
-    console.log("REFRESH TOKEN:", tokens.refresh_token);
-
-    // TEMP DEBUG:
-    // You can manually copy this from logs into Vercel env var:
-    // GOOGLE_REFRESH_TOKEN
-
-    return res.redirect(
-      "https://thesimplebrands.com/email-builder?gmail=connected"
-    );
   } catch (err) {
-    console.error("OAUTH CALLBACK ERROR:", err);
+
+    console.error(err);
 
     return res.status(500).send("OAuth failed");
+
   }
+
 }
