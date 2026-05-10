55b4bf3b0f766c99ce3416d0834e8033c5c0f769
WaldoCust <waldocust@gmail.com>
2026-05-10 14:30:47 -0400

    Update connect-gmail.js

--- api/connect-gmail.js
+++ api/connect-gmail.js
@@ -1,20 +1,22 @@
 import { google } from "googleapis";
 
 export default async function handler(req, res) {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
-  );
+    const oauth2Client = new google.auth.OAuth2(
+          process.env.GOOGLE_CLIENT_ID,
+          process.env.GOOGLE_CLIENT_SECRET,
+          process.env.GOOGLE_REDIRECT_URI
+        );
 
   const scopes = [
-    "https://www.googleapis.com/auth/gmail.send"
-  ];
+        "https://www.googleapis.com/auth/gmail.send",
+        "https://www.googleapis.com/auth/gmail.compose",
+        "https://www.googleapis.com/auth/gmail.modify"
+      ];
 
   const url = oauth2Client.generateAuthUrl({
-    access_type: "offline",
-    prompt: "consent",
-    scope: scopes
+        access_type: "offline",
+        prompt: "consent",
+        scope: scopes
   });
 
   res.redirect(url);
